7a344e8ae99589eeeeeb8e067408639caa01cc66
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2025-09-09 15:51:55 +0600

    4th commit

--- script.js
+++ script.js
@@ -21,3 +21,69 @@ var modalAddBtn = document.getElementById("modal-add-to-cart");
 var cart = [];
 var total = 0;
 var currentPlant = null;
+
+/*  normalizers */
+function getId(o) {
+  return (o && (o.id || o.plant_id || o.plantId || o.plantID)) || null;
+}
+function getName(o) {
+  return (o && (o.name || o.plant_name || o.plantName || o.title)) || "Unnamed";
+}
+function getCat(o) {
+  return (o && (o.category || o.category_name)) || "Tree";
+}
+function getPrice(o) {
+  return Number((o && o.price) || 0) || 0;
+}
+function getImg(o) {
+  return (o && (o.image || o.img || o.thumbnail || o.photo)) || "";
+}
+function getDesc(o) {
+  return (o && (o.description || o.desc || o.details)) || "";
+}
+
+function pickPlantPayload(data) {
+  return (data && (data.plant || data.plants)) || {};
+}
+
+//Load Categories
+ 
+function loadCategories() {
+  fetch(API_BASE + "/categories")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      var categories = (data && data.categories) || [];
+      showCategories(categories);
+    })
+    .catch(function () {
+      showError("categories");
+    });
+}
+
+function showCategories(categories) {
+  categoryList.innerHTML =
+    '<li><button id="all-trees" class="w-full text-left px-4 py-2 rounded-md bg-green-600 text-white">All Trees</button></li>';
+
+  categories.forEach(function (cat) {
+    var catId = cat.id || cat.category_id || cat.cat_id || "";
+    categoryList.innerHTML +=
+      '<li><button data-id="' +
+      catId +
+      '" class="w-full text-left px-4 py-2 rounded-md hover:bg-green-100">' +
+      getCat(cat) +
+      "</button></li>";
+  });
+
+  if (!categoryList._bound) {
+    categoryList.addEventListener("click", function (e) {
+      if (e.target.tagName !== "BUTTON") return;
+      var id = e.target.getAttribute("data-id");
+      if (id) loadPlantsByCategory(id);
+      else loadAllPlants();
+    });
+    categoryList._bound = true;
+  }
+}
+
